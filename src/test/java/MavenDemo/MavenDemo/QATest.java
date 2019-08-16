a69a9f5728bc3c3412c7a6c980fd93b28bd0da08
manoj.barate1@yahoo.com <manoj.barate1@yahoo.com>
2019-08-16 09:36:10 +0530

    this is my First commit

---
+++ src/test/java/MavenDemo/MavenDemo/QATest.java
@@ -0,0 +1,38 @@
+package MavenDemo.MavenDemo;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class QATest {
+	
+	@Test
+	public void sum(){
+		System.out.println("SUM");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+
+	@Test
+	public void sub(){
+		System.out.println("SUB");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void div (){
+		System.out.println("DIV");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+	@Test
+	public void MULTI(){
+		System.out.println("MULTI");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+}
